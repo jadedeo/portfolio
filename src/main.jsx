3649e19017576c1ebdf769765d1660d1aa2c72fe
jadedeo <jadedeo@gmail.com>
2025-09-17 16:39:34 -0400

    update router basename

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/portfolio">
+        <BrowserRouter basename="/">
             <ScrollToTop />
 
             <App />
